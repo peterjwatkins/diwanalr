bc5db682cfd227b131b051c692cba9fc7d024c92
Peter Watkins <Peter.Watkins@csiro.au>
2018-07-09 20:06:55 +1000

    Plot edits

--- R/g1.R
+++ R/g1.R
@@ -49,7 +49,11 @@ plot_g1 <- function(d) {
     p <- g1_spline(d)
     t_half <- approxfun(x = p, y = d$time)(0.5)
     e <- tidyr::gather(d, key = g1, Value, -time)
-    ggplot2::ggplot(e, aes(time, Value, color = g1)) + geom_point() + scale_x_log10() + labs(x = "Correlation time (s)", y = "g1(t)") +
-        geom_hline(yintercept = 0.5, linetype = "dashed", color = "blue") + geom_vline(xintercept = approxfun(x = p,
-        y = d$time)(0.5), linetype = "dashed", color = "blue")
+    ggplot2::ggplot(e, ggplot2::aes(time, Value, color = g1)) + 
+      ggplot2::geom_point() + 
+      ggplot2::scale_x_log10() + 
+      ggplot2::labs(x = "Correlation time (s)", y = "g1(t)") +
+      ggplot2::geom_hline(yintercept = 0.5, linetype = "dashed", color = "blue") + 
+      ggplot2::geom_vline(xintercept = approxfun(x = p,
+          y = d$time)(0.5), linetype = "dashed", color = "blue")
 }
