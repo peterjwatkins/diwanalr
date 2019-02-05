d60027d157186d5e921054d84ad046a79131e318
Peter Watkins <Peter.Watkins@csiro.au>
2019-02-05 14:42:41 +1100

    Removes warning note for R CMD check

---
+++ R/globals.R
@@ -0,0 +1,6 @@
+# Used to remove warnings for 
+# "no visible binding for global variable"
+#
+#' @import utils 
+utils::globalVariables(c('freq','g1','g2','msd','Modulus','Observed',
+                         'Scaled','time', 'val','Value'))
