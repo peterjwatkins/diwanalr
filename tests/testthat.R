3dd01e29a0f014ece72d9e061dd1e931c96bc31c
Peter Watkins <Peter.Watkins@csiro.au>
2019-05-30 14:50:56 +1000

    Added tests for g1 calculations

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,3 +2,20 @@ library(testthat)
 library(diwanalr)
 
 test_check("diwanalr")
+## Test performed on the calculations use model data.
+
+# The model response is taken from:
+# Coarsening and Rheology of Casein and Surfactant Foams
+# A. Saint-Jalmes, S. Marze and D. Langevin
+# Equations 4 and 5, page 276
+# Food Colloids: Interactions, Microstructure and Processing
+# ed. by Eric Dickinson
+# g2(t) ~ 1 + beta*g1(t)^2
+# g1(t) ~ exp(-2*sqrt(6*t/tm)) and
+
+# -------- R code to produce model data
+# span <- seq(-8, 1, 0.05) ; time <- 10^span
+# t_half <- 0.05
+# g2 <- exp(-2*sqrt(6*time/t_half))
+# g1 <- 1 + 0.9*g2^2
+# test <- tibble::as_tibble(cbind(time, g2))
--- tests/testthat/test_g1.R
+++ tests/testthat/test_g1.R
@@ -3,6 +3,16 @@ context("testing g1")
 library("testthat")
 
 test_that("form_g1 returns df", {
-  expect_is(form_g1(dws),  "data.frame") 
-}
-)
+  expect_is(form_g1(dws), "data.frame")
+})
+
+test_that("Low time value returns appropriate g1(t) result", {
+  expect_equal(
+    sqrt((1 + 0.9*exp(-2*sqrt(6*10^-8/0.05))^2)-1),
+    0.95, tolerance=1e-2)
+})
+
+test_that("High time value returns appropriate g1(t) result", {
+  expect_equal(sqrt((1+ exp(-2*sqrt(6*10^3/0.05)))-1), 0.000, tolerance=1e-2)
+})
+
